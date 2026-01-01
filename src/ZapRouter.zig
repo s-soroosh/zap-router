29f47cd87e75f2195e5fe5708280a77364ec8192
Soroosh <soroosh.sarabadani@gmail.com>
2026-01-01 23:30:13 +0100

    Naive implementation of ZapRouter

---
+++ src/ZapRouter.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const zap = @import("zap");
+const Self = @This();
+
+const Allocator = std.mem.Allocator;
+routes: std.StringHashMap(zap.HttpRequestFn),
+
+pub const Method = enum { GET, POST, DELETE, HEAD, PUT, PATCH, OPTION };
+var instance: *Self = undefined;
+
+pub fn init(allocator: Allocator) Self {
+    const routes = std.StringHashMap(zap.HttpRequestFn).init(allocator);
+    return .{ .routes = routes };
+}
+
+pub fn route(self: *Self, method: Method, path: []const u8, handler: zap.HttpRequestFn) !void {
+    _ = method;
+    try self.routes.put(path, handler);
+}
+pub fn dispatch(self: *Self, request: zap.Request) !void {
+    std.debug.print("you are here!!!", .{});
+    if (request.path) |path| {
+        if (self.routes.get(path)) |handler| return handler(request);
+    }
+    return request.sendBody("oops!");
+}
+
+pub fn onRequest(self: *Self) zap.HttpRequestFn {
+    instance = self;
+    const obj = struct {
+        pub fn apply(req: zap.Request) !void {
+            return dispatch(instance, req);
+        }
+    };
+    return obj.apply;
+}
