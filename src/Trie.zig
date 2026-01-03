faf1bdf10b01e67b576259d73cdb81af55629bfd
Soroosh <soroosh.sarabadani@gmail.com>
2026-01-03 01:11:16 +0100

    Add dynamic field to Node

--- src/Trie.zig
+++ src/Trie.zig
@@ -6,27 +6,32 @@ const startsWith = std.mem.startsWith;
 const endsWith = std.mem.endsWith;
 const split = std.mem.splitAny;
 
-pub const StaticNode = struct {
+pub const Node = struct {
     isAnswer: bool,
-    staticChildren: StringHashMap(*StaticNode),
-    dynamicChildren: StringHashMap(*DynamicNode),
+    staticChildren: StringHashMap(*Node),
+    dynamicChildren: StringHashMap(*Node),
+    dynamic: bool,
 
-    pub fn init(allocator: std.mem.Allocator, isAnswer: bool) !StaticNode {
-        return .{ .staticChildren = StringHashMap(*StaticNode).init(allocator), .dynamicChildren = StringHashMap(*DynamicNode).init(allocator), .isAnswer = isAnswer };
+    pub fn init(allocator: std.mem.Allocator, isAnswer: bool, dynamic: bool) !Node {
+        return .{
+            .staticChildren = StringHashMap(*Node).init(allocator),
+            .dynamicChildren = StringHashMap(*Node).init(allocator),
+            .isAnswer = isAnswer,
+            .dynamic = dynamic,
+        };
     }
 
-    pub fn deinit(self: *StaticNode) void {
+    pub fn deinit(self: *Node) void {
         self.staticChildren.clearAndFree();
         self.dynamicChildren.clearAndFree();
     }
 };
-pub const DynamicNode = struct { isAnswer: bool, staticChildren: StringHashMap(*StaticNode), dynamicChildren: StringHashMap(*DynamicNode) };
 
-_root: StaticNode,
+_root: *Node,
 allocator: std.mem.Allocator,
 
 pub fn init(allocator: std.mem.Allocator) !Self {
-    return .{ .allocator = allocator, ._root = try StaticNode.init(allocator, false) };
+    return .{ .allocator = allocator, ._root = try _createNode(allocator) };
 }
 
 pub fn deinit(self: *Self) void {
@@ -35,7 +40,7 @@ pub fn deinit(self: *Self) void {
 
 pub fn addPath(self: *Self, path: []const u8) !void {
     var parts = split(comptime u8, path, "/");
-    var root = &self._root;
+    var root = self._root;
     while (parts.next()) |part| {
         if (part.len == 0) continue;
         if (try _isDynamicNode(part)) {
@@ -43,7 +48,6 @@ pub fn addPath(self: *Self, path: []const u8) !void {
             const new_node = try _createNode(self.allocator);
             try root.dynamicChildren.put(part[1 .. part.len - 1], new_node);
         } else {
-            // static node
             if (root.staticChildren.get(part)) |node| {
                 root = node;
             } else {
@@ -56,9 +60,9 @@ pub fn addPath(self: *Self, path: []const u8) !void {
     }
 }
 
-fn _createNode(allocator: std.mem.Allocator) !*StaticNode {
-    const node = try allocator.create(StaticNode);
-    node.* = try StaticNode.init(allocator, false);
+fn _createNode(allocator: std.mem.Allocator) !*Node {
+    const node = try allocator.create(Node);
+    node.* = try Node.init(allocator, false, false);
     return node;
 }
 
@@ -93,4 +97,12 @@ test "add path with prefix overlap" {
     try trie.addPath("/part1/part2/part3");
     try trie.addPath("/part1/part4/part5");
     try std.testing.expectEqual(2, trie._root.staticChildren.get("part1").?.staticChildren.count());
+}
+
+test "path variables" {
+    var trie = try Self.init(std.heap.c_allocator);
+    defer trie.deinit();
+    try trie.addPath("/users/{userId}");
+    try std.testing.expectEqual(1, trie._root.staticChildren.get("users").?.dynamicChildren.count());
+    try std.testing.expect(trie._root.staticChildren.get("users").?.dynamicChildren.get("userId") != null);
 }
