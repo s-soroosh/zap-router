2c93a62d0a10a93b9d1754f440a7f34889d3d680
Soroosh <soroosh.sarabadani@gmail.com>
2026-01-05 17:18:38 +0100

    Remove unnecessary codes

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,4 @@
 const std = @import("std");
-const Io = std.Io;
-
 const zap = @import("zap");
 const zap_router = @import("zap_router");
 const ZapRouter = zap_router.ZapRouter;
@@ -14,24 +12,9 @@ fn sayHello(req: zap.Request) !void {
 }
 
 pub fn main() !void {
-    // In order to allocate memory we must construct an `Allocator` instance.
     var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
     defer _ = debug_allocator.deinit(); // This checks for leaks.
     const gpa = debug_allocator.allocator();
-
-    // In order to do I/O operations we must construct an `Io` instance.
-    var threaded: std.Io.Threaded = .init(gpa, .{});
-    defer threaded.deinit();
-    const io = threaded.io();
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
-    try stdout_writer.flush();
 
     var router = try ZapRouter.init(gpa);
     defer router.deinit();
