b156fd9022216c745d0311253db1b6f54eeabab8
Soroosh <soroosh.sarabadani@gmail.com>
2026-01-05 18:00:33 +0100

    Add PathVariable example

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,16 @@ const std = @import("std");
 const zap = @import("zap");
 const zap_router = @import("zap_router");
 const ZapRouter = zap_router.ZapRouter;
+const PathVariables = std.StringHashMap([]const u8);
 
-fn dispatchRequest(req: zap.Request) !void {
-    try req.sendJson("{\"message\":\"Hello dude\"}");
+fn sayHello(req: zap.Request, _: PathVariables) !void {
+    try req.sendBody("Hello");
 }
 
-fn sayHello(req: zap.Request) !void {
-    try req.sendBody("Hello");
+fn sayUserId(req: zap.Request, pv: PathVariables) !void {
+    var buf: [32]u8 = undefined;
+    const count = try std.fmt.bufPrint(&buf, "userId: {s}", .{pv.get("userId") orelse "0"});
+    try req.sendBody(count);
 }
 
 pub fn main() !void {
@@ -20,6 +23,7 @@ pub fn main() !void {
     defer router.deinit();
 
     try router.route(.GET, "/hello", sayHello);
+    try router.route(.GET, "/users/{userId}", sayUserId);
 
     var zap_server1 = zap.HttpListener.init(.{ .port = 3000, .on_request = router.onRequest(), .log = false });
 
